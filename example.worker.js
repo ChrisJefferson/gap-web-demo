65d3623a2f94776315f85a37a7d0f0fb72f258da
Chris Jefferson <caj21@st-andrews.ac.uk>
2022-10-11 20:54:58 +0100

    Fix

--- example.worker.js
+++ example.worker.js
@@ -1,7 +1,7 @@
 importScripts("https://cdn.jsdelivr.net/npm/xterm-pty@0.9.3/workerTools.js");
 
 onmessage = (msg) => {
-  importScripts(location.origin + "/gap.js");
+  importScripts(location.origin + "/gap-web-demo/gap.js");
 
   emscriptenHack(new TtyClient(msg.data));
 };
